b50a9203c53531ef0fac692aca2af12f9289d1d4
Md Rezaul Karim <iosmkarim@gmail.com>
2024-01-25 22:45:36 -0500

    upload Africa project

---
+++ Africa/Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Md Rezaul Karim on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
